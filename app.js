8a4d716ea4851fb7246a57547759cc0e1da7371b
bdarab <bdarabzand@gmail.com>
2021-06-24 23:56:11 +0200

    Add user choice functionality

--- app.js
+++ app.js
@@ -4,31 +4,36 @@ const appContainer = document.querySelector('#app');
 const title = document.createElement('h1');
 const subTitle = document.createElement('h2');
 const btnContainer = document.createElement('div');
-const Rock = document.createElement('button');
-const Paper = document.createElement('button');
-const Scissors = document.createElement('button');
+// let possibleChoices = document.querySelectorAll('button');
+let Rock = document.createElement('button');
+let Paper = document.createElement('button');
+let Scissors = document.createElement('button');
 
-title.textContent = 'Rock Paper & Scissors';
+title.textContent = 'Rock, Paper & Scissors';
 appContainer.appendChild(title);
-
 subTitle.textContent = "let's play a game against your computer";
 appContainer.appendChild(subTitle);
 appContainer.appendChild(btnContainer);
 btnContainer.appendChild(Rock);
 btnContainer.appendChild(Paper);
 btnContainer.appendChild(Scissors);
-
 btnContainer.classList.add('Btns');
+
 Rock.innerHTML = `
-<i class="fas fa-hand-rock"></i>
-<p>Rock</p>`;
+	<i class="fas fa-hand-rock" data-value="rock"><p>Rock</p></i>`;
 
 Paper.innerHTML = `
-<i class="fas fa-hand-paper"></i>
-<p>Paper</p>
-`;
+<i class="fas fa-hand-paper" data-value="paper"><p>Paper</p></i>`;
 
 Scissors.innerHTML = `
-<i class="fas fa-hand-scissors"></i>
-<p>Scissors</p>
-`;
+<i class="fas fa-hand-scissors" data-value="scissors"><p>Scissors</p></i>`;
+
+// Create functionality
+let possibleChoices = [Rock, Paper, Scissors];
+
+let userChoice = possibleChoices.forEach((possibleChoice) =>
+	possibleChoice.addEventListener('click', (e) => {
+		userChoice = e.target.getAttribute('data-value');
+		console.log(userChoice);
+	})
+);
