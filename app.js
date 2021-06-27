1ad6b5e21c686698141adf49c2c7ebb61a88428b
bdarab <bdarabzand@gmail.com>
2021-06-27 17:56:17 +0200

    add round & user & computer choices

--- app.js
+++ app.js
@@ -1,53 +1,30 @@
-// Creating DOM Elements
+const btnContainer = document.querySelector('.btnContainer');
+const h2 = document.querySelector('.result h2');
+const selections = ['rock', 'paper', 'scissors'];
 
-const appContainer = document.querySelector('#app');
-const title = document.createElement('h1');
-const subTitle = document.createElement('h2');
-const btnContainer = document.createElement('div');
-let btnChoices = document.querySelectorAll('button');
-let Rock = document.createElement('button');
-let Paper = document.createElement('button');
-let Scissors = document.createElement('button');
+// Functionality
 
-title.textContent = 'Rock, Paper & Scissors';
-appContainer.appendChild(title);
-subTitle.textContent = "let's play a game against your computer";
-appContainer.appendChild(subTitle);
-appContainer.appendChild(btnContainer);
-btnContainer.appendChild(Rock);
-btnContainer.appendChild(Paper);
-btnContainer.appendChild(Scissors);
-btnContainer.classList.add('Btn');
+let Round = () => {
+	let count = 1;
+	// add computer selection random choice
+	function computerPlay() {
+		computerCount = 0;
+		let computerSelection =
+			selections[Math.floor(Math.random() * selections.length)];
+		console.log(computerSelection);
+	}
+	// add player selection click event
+	let playerSelection = function () {
+		btnContainer.addEventListener('click', onClick);
+	};
 
-let possibleChoices = [Rock, Paper, Scissors];
+	let onClick = function (e) {
+		let li = e.target.getAttribute('data-name');
+		console.log(li);
+		computerPlay();
+		h2.textContent = 'Round: ' + count++;
+	};
 
-Rock.innerHTML = `
-	<i class="fas fa-hand-rock" value="rock"><p>Rock</p></i>`;
-
-Paper.innerHTML = `
-<i class="fas fa-hand-paper" value="paper"><p>Paper</p></i>`;
-
-Scissors.innerHTML = `
-<i class="fas fa-hand-scissors" value="scissors"><p>Scissors</p></i>`;
-
-// Create functionality
-
-let choices = possibleChoices.forEach((possibleChoice) =>
-	possibleChoice.addEventListener('click', (e) => {
-		userChoice = e.target.getAttribute('value');
-		let possibleChoice = [
-			{
-				name: 'rock',
-			},
-			{
-				name: 'Paper',
-			},
-			{
-				name: 'Scissors',
-			},
-		];
-		randomChoice = Math.floor(Math.random() * possibleChoice.length);
-		computerChoice = possibleChoice[randomChoice]['name'];
-		console.log(userChoice, computerChoice);
-	})
-);
+	document.addEventListener('DOMContentLoaded', playerSelection);
+};
+Round();
